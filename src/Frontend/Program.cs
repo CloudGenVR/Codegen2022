51df3dd527cf677eb81ef057078bbdf6ffdb43ae
Andrea Tosato <andrea.tosato@hotmail.it>
2022-01-30 20:43:49 +0100

    Upload

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://photogallerybackend.azurewebsites.net/api/") });
 
-builder.Services.AddScoped<IMinimalService, FakeMinimalService>();
+builder.Services.AddScoped<IMinimalService, MinimalService>();
 await builder.Build().RunAsync();
---
+++ src/Frontend/Services/MinimalService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Frontend.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Frontend.Services;
+
+public class MinimalService : IMinimalService
+{
+    private readonly HttpClient client;
+
+    public MinimalService(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public Task<Photo[]> SearchImagesAsync(string search) =>
+        client.GetFromJsonAsync<Photo[]>($"photos?q={search}")!;
+
+    public Task<byte[]> DownloadImageAsync(Guid photoId) =>
+        client.GetByteArrayAsync($"photos/{photoId}/image")!;
+
+    public async Task<Photo> UploadPhoto(string description, IBrowserFile imageForm)
+    {
+        await using var ms = new MemoryStream();
+        await imageForm.OpenReadStream().CopyToAsync(ms);
+        var content = new MultipartFormDataContent
+        {
+            { new ByteArrayContent(ms.GetBuffer()), "\"uploadFile\"", imageForm.Name }
+        };
+
+        var responseMessage = await client.PostAsync($"photos?description={description}", content);
+        responseMessage.EnsureSuccessStatusCode();
+        var messageText = await responseMessage.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<Photo>(messageText)!;
+    }
+}
